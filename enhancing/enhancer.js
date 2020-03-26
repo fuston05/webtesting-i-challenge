5a51f25ca72c6a9817f07ed8d402200213837a75
fuston05 <fuston05@yahoo.com>
2020-03-25 21:18:58 -0400

    tested that all return the expected properties, got 'seceed' method fully tested

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,19 +4,38 @@ module.exports = {
   repair,
   get,
 };
+/** item={
+  name: ''.
+  durability: '0-100',
+  enhancement: '0-20'
+}*/
 
 function succeed(item) {
-  return { ...item };
+  /**
+   * The item's enhancement increases by 1.
+- If the item enhancement level is 20, the enhancement level is not changed.
+- The durability of the item is not changed.
+   */
+  return { ...item, enhancement: item.enhancement+1 };
 }
 
 function fail(item) {
+  /**
+   * If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+- If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+- If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+   */
   return { ...item };
 }
 
 function repair(item) {
+  /**
+   * a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
+   */
   return { ...item };
 }
 
+// ** STRETCH ** //
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,90 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+/**
+ * item={
+  name: ''.
+  durability: '0-100',
+  enhancement: '0-20'
+}
+ */
+
+const item = {
+  name: 'scott',
+  durability: 100,
+  enhancement: 19
+}
+
+describe('enhancer.js', () => {
+  describe('the succeed() function', () => {
+    /**
+     * a `success(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement success.
+     */
+    it('.returns an obj with a name property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('name');
+    });
+
+    it('.returns an obj with a durability property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('durability');
+    });
+
+    it('.returns an obj with an enhancement property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('enhancement');
+    });
+
+    it('.returns obj with the items enhancement increased by 1', function () {
+      const enhancedItem = enhancer.succeed(item);
+      const originalEnhancement = item.enhancement;
+      expect(enhancedItem.enhancement).toEqual(originalEnhancement + 1);
+    });
+
+    it('.does not increase the enhancement beyond 20', function () {
+      expect(enhancer.succeed(item).enhancement).toBeLessThanOrEqual(20);
+    });
+
+
+
+  })//end succeed
+
+  describe('the fail() function', () => {
+    /**
+     * a `fail(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement failure.
+     */
+    it('.returns an obj with a name property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('name');
+    });
+
+    it('.returns an obj with a durability property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('durability');
+    });
+
+    it('.returns an obj with an enhancement property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('enhancement');
+    });
+
+  })//end succeed
+
+  describe('the repair() function', () => {
+    /**
+       * a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
+       */
+    it('.returns an obj with a name property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('name');
+    });
+
+    it('.returns an obj with a durability property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('durability');
+    });
+
+    it('.returns an obj with an enhancement property', function () {
+      expect(enhancer.repair(item)).toHaveProperty('enhancement');
+    });
+  })//end succeed
+
+  describe('get() function', () => {
+    it('works', () => {
+
+    });
+  })//end succeed
+
+})//end enhancer
